12eadbac1a2f53aa7608bd680253559e70ddc476
MindLone <154639326+MindLone@users.noreply.github.com>
2026-08-29 23:31:24 -0300

    feat: centralizar configuração comercial

---
+++ config.js
@@ -0,0 +1,11 @@
+window.SITE_CONFIG = Object.freeze({
+  PRECO_BASICO: "10",
+  LINK_BASICO: "",
+  PRECO_COMPLETO: "37",
+  LINK_COMPLETO: "",
+  LINK_UPGRADE: "",
+  EMAIL_SUPORTE: "academiadigital10@gmail.com",
+  WISTIA_HERO: "",
+  WISTIA_DEPOIMENTO_1: "",
+  WISTIA_DEPOIMENTO_2: ""
+});
